ca5e0afcd94d43493056dac3d0deec4ad031e317
OxanaSF <oxanatango@gmail.com>
2022-11-01 10:25:31 -0700

    add the Mixellence project to portfolio

--- src/utils/data.js
+++ src/utils/data.js
@@ -2,35 +2,39 @@ export const webDevProjects = [
    
     {
       id: 1,
+      url: "https://mixellence.netlify.app/",
+    },  
+    {
+      id: 2,
       url: "https://urban-rhythms.netlify.app/",
     },
     {
-      id: 2,
+      id: 3,
       url: "https://mysterious-forest.netlify.app/",
     },
     {
-      id: 3,
+      id: 4,
       url: "https://la-nutrition.netlify.app/",
     },
     {
-      id: 4,
+      id: 5,
       url: "https://play-rock-paper-scissors-react.netlify.app/",
     },
 
     {
-      id: 5,
+      id: 6,
       url: "https://we-live-healthy.netlify.app/",
     },
     {
-      id: 6,
+      id: 7,
       url: "https://weathervuejs.netlify.app/",
     },
     {
-      id: 7,
+      id: 8,
       url: "https://tei-artworks.netlify.app/"
     },
     {
-      id: 8,
+      id: 9,
       url: "https://fylo-react-8-16.netlify.app/"
     }
   ];
