d3fe5003fb387550f2b465c1c5669a3e8c01b9a1
OxanaSF <oxanatango@gmail.com>
2022-12-19 20:23:34 -0800

    add a project to portfolio

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,40 +1,44 @@
 export const webDevProjects = [
+  {
+    id: 1,
+    url: "https://main.d2dvl9mudwuexj.amplifyapp.com/",
+  },  
    
     {
-      id: 1,
+      id: 2,
       url: "https://mixellence.netlify.app/",
     },  
     {
-      id: 2,
+      id: 3,
       url: "https://urban-rhythms.netlify.app/",
     },
     {
-      id: 3,
+      id: 4,
       url: "https://mysterious-forest.netlify.app/",
     },
     {
-      id: 4,
+      id: 5,
       url: "https://la-nutrition.netlify.app/",
     },
     {
-      id: 5,
+      id: 6,
       url: "https://play-rock-paper-scissors-react.netlify.app/",
     },
 
     {
-      id: 6,
+      id: 7,
       url: "https://we-live-healthy.netlify.app/",
     },
     {
-      id: 7,
+      id: 8,
       url: "https://weathervuejs.netlify.app/",
     },
     {
-      id: 8,
+      id: 9,
       url: "https://tei-artworks.netlify.app/"
     },
     {
-      id: 9,
+      id: 10,
       url: "https://fylo-react-8-16.netlify.app/"
     }
   ];
